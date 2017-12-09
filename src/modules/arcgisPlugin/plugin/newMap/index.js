1f0bdec8195b8dca21c44eab235f3051db959921
sherrychen <1365825528@qq.com>
2017-12-09 14:27:43 +0800

    加载本地资源

--- src/modules/arcgisPlugin/plugin/newMap/index.js
+++ src/modules/arcgisPlugin/plugin/newMap/index.js
@@ -6,12 +6,25 @@ var comm = Vue.extend({
     template: template,
     data: function () {
         return {
-            isExpand:false
+            isExpand:false,
+            indexId:-1,
+            mapTypeList:[
+                {
+                    name:'影像图',
+                    class:'earth'
+                },{
+                    name:'地图',
+                    class:'normal'
+                }
+            ]
         }
     },
     methods: {
         toggleClass:function(status){
             this.isExpand = status;
+        },
+        highLight:function (index) {
+            this.indexId = index;
         }
     },
     mounted: function () {
