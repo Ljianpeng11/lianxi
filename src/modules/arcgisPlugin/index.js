df0f95d4f780c04c4f2f0d320253ac1ef8d311da
Thz-02 <415675326@qq.com>
2017-12-09 12:47:52 +0800

    增加首页地图信息展示栏

--- src/modules/arcgisPlugin/index.js
+++ src/modules/arcgisPlugin/index.js
@@ -4,28 +4,21 @@ var loginCtrl = require('../../controllers/loginController');
 var eventHelper = require('../../utils/eventHelper');
 var toolBar = require('./plugin/toolBar/toolBar');
 var mapType = require('./plugin/mapType/mapType');
-var flexMapLegend = require('./plugin/flexMapLegend');
+var newMap = require('./plugin/newMap');
+var layerList = require('./plugin/layerList');
 var global = require('./plugin/global');
 var facilityController = require('controllers/facilityController');
-var arcgisHelper = require('./plugin/arcgisExpand/arcgis-load-map');
+var infoWindow = require('./plugin/infoWindow');
 var rightPanel = require('modules/rightPanel');
-var infoWindow = require('modules/arcgisPlugin/plugin/infoWindow');
-var initBaseMap = function () {
-    //init map
-    var layerURL = 'http://112.74.51.12:6080/arcgis/rest/services/hwShow201705/MapServer';
-    var centerX = 113.32526513187868;
-    var centerY = 23.145541015892572;
-    var map = arcgisHelper.tdWmtsServer(layerURL, centerX, centerY);
-    return map;
-}
+var mapHelper = require('utils/mapHelper');
+var infoBoard = require('modules/emergencyRescue/infoBoard');
+var tabModel = require('controllers/model/appTabModel');
+var retrospectDetail = require('modules/retrospectDetail');
+
 var initPlugin = function (facilityArr, self) {
     global.init();
     facilityController.getAllFacilityType(function (list) {
         self.$refs.mapLegend.init(list);
-        // list.forEach(function (station) {
-        //     facilityArr[station.facilityTypeName] = station.facilitys;
-        //     arcgisHelper.createPoints(station);
-        // })
     });
 }
 
@@ -34,20 +27,124 @@ var comm = Vue.extend({
     template: template,
     data: function () {
         return {
-            weatherImg: './img/weather.png',
-            radarImg: '',
             message: '',
             roleName: '',
             userName: '',
-            dialogVisible: false,
-            showWeatherReportPanel: false,
             loginSuccess: false,
             detailOpen: false,
             facility: '',
-            showtools: false
+            showtools: false,
+            leftMap: {},
+            iscreateSymbol: false,
+            iscreateSymbols: false,
+            isCreatePolygon: false,
+            isCreateLine: false,
+            layer: '',
+            layers: [],
+            lineLayers: [],
+            drawGraphics: [],
+            drawPointGraphics: [],
+            drawLineGraphics: [],
+            polygonId: 1,
+            baseMap:{},
+            graLayer:{},
+            graphics:[],
+            dialogVisible:false,
+            showWeatherReportPanel:false,
+            weatherImg:"",
+            radarImg:""
         }
     },
     methods: {
+        //初始化地图
+        initBaseMap: function () {
+            var layerURL = 'http://112.74.51.12:6080/arcgis/rest/services/hwShow201705/MapServer';
+
+            var centerX = 117.80633295234286;
+            var centerY = 37.18682244869559;
+            var zoom = 10;
+            var currentMap = {};
+            var currentView = {};
+            var self = this;
+            var apiInstance = mapHelper.getInstance();
+
+            var map = mapHelper.initSuperMap("mapDiv", centerX, centerY, zoom, function (map, view) {
+                    self.baseMap = map;
+                    var apiInstance = mapHelper.getInstance();
+                    self.graLayer = apiInstance.createGraphicsLayer(self.baseMap,'graphicLayer');
+                    currentMap = map;
+                    currentView = view;
+                    //apiInstance.createMapImageLayer(currentMap, layerURL, 'haimianlayer');
+                    apiInstance.createMapImageLayer(currentMap, 'http://192.168.0.213:6080/arcgis/rest/services/gz1918pipe/gz1918Pip/MapServer', 'lineLayer');
+                    mapHelper.registerMapTool(view, 'draw-line', 'top-right', function () {
+                        var graphiceLayer = apiInstance.createGraphicsLayer(currentMap, 'testLayer');
+                        mapHelper.createPolyline(graphiceLayer, [[113.32397997379353, 23.107584714889605], [113.32745611667683, 23.107584714889605]], {
+                            color: [226, 119, 40],
+                            width: 4
+                        })
+                    });
+                    self.graLayer.on('layerview-create',function(evt){
+                        var graView = evt.view;
+                        var graLayerView = evt.layerView;
+                        var layerId = evt.layerView.layer.id;
+                        var getMap={};
+                        getMap.map = map;
+                        getMap.view = view;
+                        eventHelper.emit('get-map', getMap);
+                        graView.on('click',function(event){
+                            graView.hitTest(event).then(function(response){
+                                var graphic = response.results[0].graphic;
+                                var attributes = graphic.attributes;
+                                if(attributes.facilityType == 'IP'){
+                                    eventHelper.emit('open-facilityInfo-dialog',attributes);
+                                    return;
+                                }
+                                // mapHelper.setCenter(graView, evt.mapPoint.x, evt.mapPoint.y);
+                                if(layerId === 'graphicLayer'){
+                                    self.$refs.rightPanel.open(attributes.item, attributes.facilityTypeName);
+                                    return;
+                                }
+                            });
+                        });
+                    });
+                }
+            );
+            return map;
+        },
+        createPoint:function(legend,subFacilities){
+            subFacilities.forEach(function (item) {
+                var icon = !!legend.icon ? legend.icon : legend.facilityTypeName;
+                var newIcon = './img/toolbar/huawei-' + icon + '.png';
+                item.fid = 'f' + legend.id;
+                var imgObj = {
+                    url: newIcon,
+                    width: "30px",
+                    height: "36px"
+                };
+                var textObj = {
+                    color:'red',
+                    text:item.name,
+                    yoffset:-18,
+                    verticalAlignment:'top',
+                    font:{
+                        size:12
+                    }
+                };
+                var attributes = {
+                    'item':item,
+                    'facilityTypeName':legend.facilityTypeName,
+                    'id':item.fid
+                };
+                var graphic = mapHelper.createPictureMarkSymbol(this.graLayer, item.x, item.y, imgObj,attributes);
+                this.graphics.push(graphic);
+                this.graphics.push(mapHelper.createTextSymbol(this.graLayer,item.x,item.y,textObj));
+            }.bind(this));
+            this.facilityArr[legend.facilityTypeName] = {
+                graphics:this.graphics,
+                data:subFacilities,
+                layer:this.graLayer
+            };
+        },
         startPlan: function () {
             this.dialogVisible = false;
             var h = this.$createElement;
@@ -56,94 +153,99 @@ var comm = Vue.extend({
                 message: h('i', {style: 'color: teal'}, '请把易涝点水位监测，窨井水位监测的数据采集频率设置为10s/次')
             });
 
-        },
-        toggleSearch: function () {
-            eventHelper.emit('openPointSearch');
-            // eventHelper.emit('app-car-illegal');
-            // eventHelper.emit('app-car-cases');
-            // eventHelper.emit('app-car-pollution');
         }
     },
     mounted: function () {
+        //加载设备
         this.facilityArr = {};
         initPlugin(this.facilityArr, this);
         var self = this;
-        var map = initBaseMap();
-        eventHelper.emit('mapCreated', map);
-        this.$on('openMapLegend', function (legend) {
+        //初始化地图
+        this.initBaseMap();
+        eventHelper.on('openMapLegend', function (legend) {
             eventHelper.emit('loading-start');
-            //console.log(legend);
+            console.log(legend);
             if (!!legend.showIcon) {
                 var cacheFacilities = self.facilityArr[legend.facilityTypeName];
                 if (!!cacheFacilities && cacheFacilities.length > 0) {
-                    arcgisHelper.createPoints(cacheFacilities, legend);
+                    // arcgisHelper.createPoints(cacheFacilities, legend, true);
                     eventHelper.emit('loading-end');
                 } else {
                     facilityController.getFacilityByType(legend.id, function (subFacilities) {
-                        switch(legend.facilityTypeName){
-                            case 'WD' :
-                                subFacilities.forEach(function (subFacility) {
-                                    subFacility.icon = './css/images/huawei-yj.png'
-                                });
-                                break;
-                            case 'WP' :
-                                subFacilities.forEach(function (subFacility) {
-                                    subFacility.icon = './css/images/huawei-yld.png'
-                                });
-                                break;
-                            case 'RTU' :
-                                subFacilities.forEach(function (subFacility) {
-                                    subFacility.icon = './css/images/huawei-ylz.png'
-                                });
-                                break;
-                            default :
-                                subFacilities.forEach(function (subFacility) {
-                                    subFacility.icon = './css/images/huawei-yl.png'
-                                });
-                                break;
-                        }
-                        var graLayer = arcgisHelper.createPoints(subFacilities, legend);
-                        self.facilityArr[legend.facilityTypeName] = {
-                            data: subFacilities,
-                            layer: graLayer
-                        };
+                        // if (legend.facilityTypeName == 'CP') {
+                        //     this.reportPointInterval = setInterval(function () {
+                        //         var cacheFacilities = self.facilityArr[legend.facilityTypeName];
+                        //         facilityController.getFacilityByType(legend.id, function (subFacilities) {
+                        //             debugger;
+                        //             if (cacheFacilities.data.length == subFacilities.length) {
+                        //
+                        //             } else {
+                        //                 self.$notify({
+                        //                     title: '提示信息',
+                        //                     message: '新增巡查上报点',
+                        //                     type: 'warning'
+                        //                 });
+                        //                 var items = [{
+                        //                     title: '上报时间',
+                        //                     content: moment().format('MM-DD HH:mm:ss', new Date())
+                        //                 }, {
+                        //                     title: '案件类型',
+                        //                     content: subFacilities[subFacilities.length - 1].name
+                        //                 }]
+                        //                 eventHelper.emit('alert-point', [{
+                        //                     items: items,
+                        //                     x: subFacilities[subFacilities.length - 1].x,
+                        //                     y: subFacilities[subFacilities.length - 1].y
+                        //                 }]);
+                        //                 mapHelper.removeGraphics(self.facilityArr[legend.facilityTypeName].layer,self.facilityArr[legend.facilityTypeName].graphics);
+                        //                 self.createPoint(legend,subFacilities);
+                        //                 cacheFacilities = self.facilityArr[legend.facilityTypeName];
+                        //             }
+                        //         }.bind(this));
+                        //     }, 1000);
+                        // } else {
+                        //     if (!!self.reportPointInterval) {
+                        //         clearInterval(self.reportPointInterval);
+                        //         eventHelper.emit('alert-point-close');
+                        //     }
+                        // }
+                        self.createPoint(legend,subFacilities);
                         eventHelper.emit('loading-end');
                     });
                 }
             } else {
-                arcgisHelper.removePoints(self.facilityArr[legend.facilityTypeName]);
+                var layer = self.facilityArr[legend.facilityTypeName].layer;
+                var graphics = self.facilityArr[legend.facilityTypeName].graphics;
+                mapHelper.removeGraphics(layer,graphics);
                 eventHelper.emit('loading-end');
             }
-        });
+        }.bind(this));
         eventHelper.on('subFacility-clicked', function (point) {
-            // console.log(point);
-            map.centerAt([parseFloat(point.center[0]) + 0.001, point.center[1]]);
+            console.log(point);
+            map.centerAt([parseFloat(point.center[0]) + 0.005, point.center[1]]);
             this.$refs.rightPanel.open(point.item, point.facilityTypeName);
         }.bind(this));
-        eventHelper.on('startPlan', function () {
-            setTimeout(function () {
-                this.dialogVisible = true;
-                this.showWeatherReportPanel = true;
-                var i = 1;
-                this.radarCounter = setInterval(function () {
-                    if (i > 47) {
-                        i = 1;
-                    }
-                    this.radarImg = './img/radar/' + i + '.png';
-                    i++;
-                }.bind(this), 500);
-            }.bind(this), 2000);
+        eventHelper.on('carDetail-clicked', function (point) {
+            console.log(point);
+            this.$refs.carDetail.open(point.item);
         }.bind(this));
-        eventHelper.on('stopPlan', function () {
-            this.showWeatherReportPanel = false;
-            clearInterval(this.radarCounter);
+        eventHelper.on('changeMap', function () {
+            this.baseMap.layers.items[0].visible=false;
+            this.baseMap.layers.items[1].visible=true;
+        }.bind(this));
+        eventHelper.on('changeMap1', function () {
+            this.baseMap.layers.items[1].visible=false;
+            this.baseMap.layers.items[0].visible=true;
+        }.bind(this));
 
-        }.bind(this));
     },
     components: {
-        'right-panel': rightPanel,
-        'flex-map-legend': flexMapLegend,
-        'info-window': infoWindow
+        'layer-list':layerList,
+        'info-window': infoWindow,
+        'right-panel':rightPanel,
+        'retrospect-detail':retrospectDetail,
+        'info-board':infoBoard,
+        'new-map':newMap
     }
 });
 module.exports = comm;
