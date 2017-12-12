b0c76d927c458d960efacebf5a054b62ad4377e8
Thz-02 <415675326@qq.com>
2017-12-12 16:04:44 +0800

    完善地图定位和设备列表

--- src/modules/onlineMonitor/mapPlugin/deviceList/index.js
+++ src/modules/onlineMonitor/mapPlugin/deviceList/index.js
@@ -10,8 +10,7 @@ var comm = Vue.extend({
         return {
             isOpenList:false,
             highLightIndex:-1,
-            deviceList:[
-                {
+            deviceList:[{
                     title:'五华区公众雨量监测点',
                     status:0,
                     voltage:'low',
@@ -23,8 +22,7 @@ var comm = Vue.extend({
                     collect:0,
                     x:117.8216341936,
                     y:37.1701173675
-                },
-                {
+                },{
                     title:'市体育馆',
                     status:1,
                     voltage:'middle',
@@ -36,8 +34,7 @@ var comm = Vue.extend({
                     collect:0,
                     x:117.8563419346,
                     y:37.1701172675
-                },
-                {
+                },{
                     title:'普吉路与小路沟交叉口',
                     status:2,
                     voltage:'high',
@@ -49,8 +46,7 @@ var comm = Vue.extend({
                     collect:1,
                     x:117.8143419346,
                     y:37.1603759513
-                },
-                {
+                },{
                     title:'滇缅大道戛纳小镇旁',
                     status:3,
                     voltage:'middle',
@@ -62,8 +58,7 @@ var comm = Vue.extend({
                     collect:1,
                     x:117.8200419346,
                     y:37.1706172675
-                },
-                {
+                },{
                     title:'海源学院正门口',
                     status:3,
                     voltage:'middle',
@@ -75,8 +70,7 @@ var comm = Vue.extend({
                     collect:0,
                     x:1,
                     y:2
-                },
-                {
+                },{
                     title:'西二环春苑小区对面',
                     status:3,
                     voltage:'middle',
@@ -89,7 +83,8 @@ var comm = Vue.extend({
                     x:1,
                     y:2
                 }
-            ]
+            ],
+            selectIitem:{}
         }
     },
     methods: {
@@ -103,18 +98,29 @@ var comm = Vue.extend({
             }
         },
         openMapWindow:function(index){
+            this.selectIitem = this.deviceList[index];
             this.highLightIndex = index;
             if(this.baseView === 'detailView'){
 
             }else{
-                var mapPoint = mapHelper.createPoint(this.deviceList[index].x,this.deviceList[index].y);
-                mapHelper.setCenter(this.baseView,this.deviceList[index].x,this.deviceList[index].y,16);
-                this.baseView.popup.open({
-                    location: mapPoint,
-                    title: this.deviceList[index].title,
-                    content: $(this.$el.children[1].children[index]).find(".listContent").clone()[0]
-                });
+                this.$nextTick(function () {
+                    var mapPoint = mapHelper.createPoint(this.deviceList[index].x,this.deviceList[index].y);
+                    mapHelper.setCenter(this.baseView,this.deviceList[index].x,this.deviceList[index].y,16);
+                    var cloneObj = $(this.$el.children[2].children[0]).clone();
+                    cloneObj.find(".mapInfoDetailBtn").click(function(){
+                        this.detailView(this.selectIitem)
+                    }.bind(this));
+                    this.baseView.popup.open({
+                        location: mapPoint,
+                        title: this.deviceList[index].title,
+                        content :cloneObj[0]
+                    });
+                    this.baseView.popup.class="gaoqingPopup";
+                }.bind(this));
             }
+        },
+        detailView:function(selectItem){
+            debugger;
         }
     },
     mounted: function () {
