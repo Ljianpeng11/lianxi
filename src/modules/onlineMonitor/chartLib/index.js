c659b5c13ed7447dba953df44d2c3b89b3b6be8f
sherrychen <1365825528@qq.com>
2017-12-17 23:53:36 +0800

    调整

--- src/modules/onlineMonitor/chartLib/index.js
+++ src/modules/onlineMonitor/chartLib/index.js
@@ -21,221 +21,224 @@ var comm = Vue.extend({
             deviceInfo:{},
             options:{}
         }
-
+    },
+    watch:{
+        chartOptions:function(val){
+            this.loadChart('#' + this.chartId,val);
+        }
     },
     created(){
-        var self = this;
-        switch(this.chartOptions.type){
-            case 'YLChart':
-                this.options = {
-                    backgroundColor: '#fff',
-                    tooltip: {
-                        trigger: 'axis'
-                    },
-                    grid: {
-                        left: '8%',
-                        right: '8%',
-                        bottom: '15%',
-                        top: '20%'
-                    },
-                    xAxis: [
-                        {
-                            type: 'category',
-                            boundaryGap: false,
-                            axisLine: {
-                                onZeroAxisIndex: 1
-                            },
-                            data: this.chartOptions.xData
-                        }
-                    ],
-                    yAxis: [
-                        {
-                            name: '测量液位(m)',
-                            type: 'value',
-                            max: 4,
-                            splitNumber: 4,
-                            axisLine: {
-                                show: false
-                            }
-                        },{
-                            name: '降雨量(mm)',
-                            type: 'value',
-                            max: 120,
-                            inverse: true,
-                            nameLocation: 'start',
-                            splitNumber: 4,
-                            axisLine: {
-                                show: false
-                            }
-                        }
-                    ],
-                    visualMap: {
-                        top: 10,
-                        right: 10,
-                        seriesIndex:0,
-                        showLabel:false,
-                        show:false,
-                        pieces: [{
-                            gt: 0,
-                            lte: 0.5,
-                            color: '#2f91e4'
-                        }, {
-                            gt: 0.5,
-                            lte: 0.8,
-                            color: '#f2b817'
-                        }, {
-                            gt: 0.8,
-                            color: '#fe5240'
-                        }],
-                        outOfRange: {
-                            color: '#999'
-                        }
-                    },
-                    series: [
-                        {
-                            name: '测量液位(m)',
-                            type: 'line',
-                            symbol: 'diamond',
-                            symbolSize: 4,
-                            tooltip: {
-                                trigger: 'axis'
-                            },
-                            smooth: true,
-                            // itemStyle: {
-                            //     normal: {
-                            //         color: 'rgba(67,67,72, 1)',
-                            //         lineStyle: {
-                            //             color: 'rgba(67,67,72, 0.8)'
-                            //         },
-                            //         areaStyle: {
-                            //             color: 'rgba(67,67,72, 0.8)'
-                            //         }
-                            //     }
-                            // },
-                            markLine: {
-                                lineStyle: {
-                                    normal: {
-                                        color: '#fe5240',
-                                        width: 1,
-                                        type: 'solid'
-                                    }
-                                },
-                                symbolSize: 0,
-                                data: [{
-                                    yAxis: 0.5,
-                                    label: {
-                                        normal: {
-                                            show: true,
-                                            formatter: '预警',
-                                            position:'start'
-                                        }
-                                    },
-                                    lineStyle:{
-                                        normal:{
-                                            color:'#f2b817'
-                                        }
-                                    }
-                                }, {
-                                    yAxis: 0.8,
-                                    label: {
-                                        normal: {
-                                            show: true,
-                                            formatter: '报警',
-                                            position:'start'
-                                        }
-                                    }
-                                }],
-                                label: {
-                                    normal: {
-                                        formatter: '{b}:{d}'
-                                    }
-                                }
-                            },
-                            data: this.chartOptions.yData1
-                        },{
-                            name: '降雨量(mm)',
-                            type: 'bar',
-                            barWidth:'10%',
-                            // symbolSize: 4,
-                            tooltip: {
-                                trigger: 'axis'
-                            },
-                            yAxisIndex: 1,
-                            // smooth: true,
-                            itemStyle: {
-                                normal: {
-                                    color: 'rgba(124,181,236, 1)',
-                                    lineStyle: {
-                                        color: 'rgba(124,181,236, 0.8)'
-                                    },
-                                    areaStyle: {
-                                        color: 'rgba(124,181,236, 0.8)'
-                                    }
-                                }
-                            },
-                            data: this.chartOptions.yData2
-                        }
-                    ]
-                };
-                break;
-            case 'pieChart':
-                this.options = {
-                    color:this.chartOptions.color,
-                    title : {
-                        subtext: this.chartOptions.subtext,
-                        left:'10px',
-                        top:'-20px'
-                    },
-                    tooltip : {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: 'vertical',
-                        left: '2%',
-                        y:'20%',
-                        data: (function(){
-                            var arr=[];
-                            self.chartOptions.data.forEach(function(val){
-                                arr.push(val.name);
-                            });
-                            return arr;
-                        })()
-                    },
-                    series : [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius : '45%',
-                            center: ['60%', '70%'],
-                            data:self.chartOptions.data,
-                            itemStyle: {
-                                emphasis: {
-                                    shadowBlur: 10,
-                                    shadowOffsetX: 0,
-                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                }
-                            }
-                        }
-                    ]
-                };
-                break;
-            default:break;
-        }
-        //初始化图表
-        this.loadChart('#' + this.chartId);
+
     },
     methods: {
-        loadChart:function(dom){
+        loadChart:function(dom,data){
             this.$nextTick(function () {
                 this.myChart = echarts.init($(dom)[0]);
+                var self = this;
+                switch(data.type){
+                    case 'YLChart':
+                        this.options = {
+                            backgroundColor: '#fff',
+                            tooltip: {
+                                trigger: 'axis'
+                            },
+                            grid: {
+                                left: '8%',
+                                right: '8%',
+                                bottom: '15%',
+                                top: '20%'
+                            },
+                            xAxis: [
+                                {
+                                    type: 'category',
+                                    boundaryGap: false,
+                                    axisLine: {
+                                        onZeroAxisIndex: 1
+                                    },
+                                    data: data.xData
+                                }
+                            ],
+                            yAxis: [
+                                {
+                                    name: '测量液位(m)',
+                                    type: 'value',
+                                    max: 4,
+                                    splitNumber: 4,
+                                    axisLine: {
+                                        show: false
+                                    }
+                                },{
+                                    name: '降雨量(mm)',
+                                    type: 'value',
+                                    max: 120,
+                                    inverse: true,
+                                    nameLocation: 'start',
+                                    splitNumber: 4,
+                                    axisLine: {
+                                        show: false
+                                    }
+                                }
+                            ],
+                            visualMap: {
+                                top: 10,
+                                right: 10,
+                                seriesIndex:0,
+                                showLabel:false,
+                                show:false,
+                                pieces: [{
+                                    gt: 0,
+                                    lte: 0.5,
+                                    color: '#2f91e4'
+                                }, {
+                                    gt: 0.5,
+                                    lte: 0.8,
+                                    color: '#f2b817'
+                                }, {
+                                    gt: 0.8,
+                                    color: '#fe5240'
+                                }],
+                                outOfRange: {
+                                    color: '#999'
+                                }
+                            },
+                            series: [
+                                {
+                                    name: '测量液位(m)',
+                                    type: 'line',
+                                    symbol: 'diamond',
+                                    symbolSize: 4,
+                                    tooltip: {
+                                        trigger: 'axis'
+                                    },
+                                    smooth: true,
+                                    // itemStyle: {
+                                    //     normal: {
+                                    //         color: 'rgba(67,67,72, 1)',
+                                    //         lineStyle: {
+                                    //             color: 'rgba(67,67,72, 0.8)'
+                                    //         },
+                                    //         areaStyle: {
+                                    //             color: 'rgba(67,67,72, 0.8)'
+                                    //         }
+                                    //     }
+                                    // },
+                                    markLine: {
+                                        lineStyle: {
+                                            normal: {
+                                                color: '#fe5240',
+                                                width: 1,
+                                                type: 'solid'
+                                            }
+                                        },
+                                        symbolSize: 0,
+                                        data: [{
+                                            yAxis: 0.5,
+                                            label: {
+                                                normal: {
+                                                    show: true,
+                                                    formatter: '预警',
+                                                    position:'start'
+                                                }
+                                            },
+                                            lineStyle:{
+                                                normal:{
+                                                    color:'#f2b817'
+                                                }
+                                            }
+                                        }, {
+                                            yAxis: 0.8,
+                                            label: {
+                                                normal: {
+                                                    show: true,
+                                                    formatter: '报警',
+                                                    position:'start'
+                                                }
+                                            }
+                                        }],
+                                        label: {
+                                            normal: {
+                                                formatter: '{b}:{d}'
+                                            }
+                                        }
+                                    },
+                                    data: data.yData1
+                                },{
+                                    name: '降雨量(mm)',
+                                    type: 'bar',
+                                    barWidth:'10%',
+                                    // symbolSize: 4,
+                                    tooltip: {
+                                        trigger: 'axis'
+                                    },
+                                    yAxisIndex: 1,
+                                    // smooth: true,
+                                    itemStyle: {
+                                        normal: {
+                                            color: 'rgba(124,181,236, 1)',
+                                            lineStyle: {
+                                                color: 'rgba(124,181,236, 0.8)'
+                                            },
+                                            areaStyle: {
+                                                color: 'rgba(124,181,236, 0.8)'
+                                            }
+                                        }
+                                    },
+                                    data: data.yData2
+                                }
+                            ]
+                        };
+                        break;
+                    case 'pieChart':
+                        this.options = {
+                            color:data.color,
+                            title : {
+                                subtext: data.subtext,
+                                left:'10px',
+                                top:'-20px'
+                            },
+                            tooltip : {
+                                trigger: 'item',
+                                formatter: "{a} <br/>{b} : {c} ({d}%)"
+                            },
+                            legend: {
+                                orient: 'vertical',
+                                left: '2%',
+                                y:'20%',
+                                data: (function(){
+                                    var arr=[];
+                                    data.data.forEach(function(val){
+                                        arr.push(val.name);
+                                    });
+                                    return arr;
+                                })()
+                            },
+                            series : [
+                                {
+                                    name: '访问来源',
+                                    type: 'pie',
+                                    radius : '45%',
+                                    center: ['60%', '70%'],
+                                    data:data.data,
+                                    itemStyle: {
+                                        emphasis: {
+                                            shadowBlur: 10,
+                                            shadowOffsetX: 0,
+                                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                        }
+                                    }
+                                }
+                            ]
+                        };
+                        break;
+                    default:break;
+                }
                 this.myChart.setOption(this.options);
                 eventHelper.on('toggle-menu', function () {
                     setTimeout(function () {
                         this.myChart.resize();
                     }.bind(this), 1000)
                 }.bind(this));
-                if(this.chartOptions.type === 'YLChart'){
+                if(data.type === 'YLChart'){
                     this.refreshYLChart();
                 }
             }.bind(this));
@@ -256,14 +259,13 @@ var comm = Vue.extend({
                 this.myChart.setOption(this.options,true);
             }.bind(this),2000);
         },
-        reloadChart:function(){
-            this.myChart.setOption(this.options,true);
+        reloadChart:function(data){
+            this.chartOptions = data;
         }
     },
     mounted: function () {
-        this.$on('reloadChart',function(){
-            this.myChart.setOption(this.options,true);
-        }.bind(this));
+        //初始化图表
+        this.loadChart('#' + this.chartId,this.chartOptions);
     },
     components: {}
 });
