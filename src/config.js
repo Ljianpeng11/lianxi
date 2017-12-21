0c917c8733f260b24844d6ef8b38b818aa51e0b2
hesi.xian <hesi.xian@163.com>
2017-12-21 20:54:13 +0800

    覆盖配置

--- src/config.js
+++ src/config.js
@@ -1,16 +1,10 @@
 define(function () {
     return {
-        //ewater服务端url
-        // basicURL: 'http://120.77.246.153:35978'
-        //高青服务端
-        // basicURL: 'http://192.168.0.240:32504',
-        // huaweiURL:'http://11.0.204.21:32204',
-        //1918服务端
-        // basicURL: 'http://192.168.0.240:32504'
-        // basicURL: 'http://120.77.246.153:32504'
-        // basicURL: 'http://139.159.246.230:32904'
-        // basicURL: 'http://139.159.246.230:33104'
+        //华为云ewater_gq开发环境
+        // basicURL: 'http://139.159.246.230:32904',
+        //高青ewater_gq生产环境
+        huaweiURL: 'http://11.0.204.21:32904',
+        //本地ewater_gq开发环境
         basicURL: 'http://localhost:32904'
-        // basicURL: 'http://192.168.0.115:32904'
     }
 });
